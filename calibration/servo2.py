7f0196e2552b556c66440a9258ba993ac49e2c2e
Dennis Núñez Fernández <dennishnf@gmail.com>
2021-12-13 15:18:27 +0100

    updating readme

---
+++ calibration/servo2.py
@@ -0,0 +1,36 @@
+from smbus import SMBus
+from PCA9685 import PWM
+import time
+
+fPWM = 50
+i2c_address = 0x40 # (standard) adapt to your module
+channels = [0] # adapt to your wiring
+a = 7.7 # adapt to your servo
+b = 2.9  # adapt to your servo
+
+def setup():
+    global pwm
+    bus = SMBus(1) # Raspberry Pi revision 2
+    pwm = PWM(bus, i2c_address)
+    pwm.setFreq(fPWM)
+
+def setDirection(direction):
+    duty = a / 180 * direction + b
+    for i in channels:
+        pwm.setDuty(i,duty)
+    print "direction =", direction, "-> duty =", duty
+    time.sleep(0.4) # allow to settle
+
+print "starting"
+setup()
+#for direction in range(0, 181, 10):
+#    setDirection(direction)
+#direction = 0
+setDirection(0)
+print "should be on 180"
+time.sleep(1)
+setDirection(90)
+time.sleep(1)
+print "should end on 90"
+setDirection(180)
+print "done"
